46bdf08e7e43de919adecf664db5cb6dc1c20cb0
ahmad-imran <imrannobine@gmail.com>
2023-07-27 00:21:36 +0600

    updated

--- src/components/weather/temp.jsx
+++ src/components/weather/temp.jsx
@@ -1,22 +1,131 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const Temp = () => {
-    return (
-        <>
-            <div className="wrap">
-                <div className="search">
-                    <input type="search"
-                        placeholder="search..."
-                        autoFocus
-                        id="search"
-                        className="searchTerm"
-                    />
+  const [searchValue, setSearchValue] = useState("Dhaka");
 
-                </div>
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&metricunits=&appid=e77b4ad65a27c77404bb1aa88ac59bf2`;
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* Temp Card */}
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={"wi wi-day-sunny"}></i>
+        </div>
+
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>25.5</span>
+          </div>
+
+          <div className="description">
+            <div className="weatherCondition">sunny</div>
+            <div className="place">Dhaka,Bangladesh</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+
+        {/* 4 column section */}
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 <br />
+                sunset
+              </p>
             </div>
-        </>
-    );
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 <br />
+                Humidity
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:19 <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default Temp;
--- src/components/weather/weathercard.jsx
+++ src/components/weather/weathercard.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const weathercard = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  return <div></div>;
 };
 
 export default weathercard;
